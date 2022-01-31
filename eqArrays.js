5a83f19563ecceb8c20a59360792142b3095b486
Regan Setter <regansetter@gmail.com>
2022-01-31 12:41:50 -0700

    Implemented eqArrays function. Modulized.

---
+++ eqArrays.js
@@ -0,0 +1,14 @@
+const assertEqual = require('./assertEqual');
+
+const eqArrays = (arr1, arr2) => { //
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+module.exports = eqArrays;
+
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
