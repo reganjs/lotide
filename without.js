159ee5ac0eb7641cf70084eae8c522432361cbc7
Regan Setter <regansetter@gmail.com>
2022-02-01 11:14:21 -0700

    Reformatted 'without' code. Some syntax errors were apparent.

--- without.js
+++ without.js
@@ -11,6 +11,7 @@ const assertArrayEqual = (actual, expected) => {
   if (eqArrays(actual === expected)) {
     console.log(` 👍🏻👍🏻👍🏻 Assertion Passed: , ${actual}, = , ${expected}`);
   }
+
   else {
     console.log(` 👎🏻👎🏻👎🏻 Assertion Failed: , ${actual}, != , ${expected}`);
   }
