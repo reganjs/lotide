4db802d4eaa36ded8de12334c7a7870ba382040f
Regan Setter <regansetter@gmail.com>
2022-02-01 11:10:21 -0700

    Implemented 'without' function that returns a subset of a given array, removing any unwanted elements. Commented in previous work. Modulized.

---
+++ without.js
@@ -0,0 +1,37 @@
+/* const eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArrayEqual = (actual, expected) => {
+  if (eqArrays(actual === expected)) {
+    console.log(` 👍🏻👍🏻👍🏻 Assertion Passed: , ${actual}, = , ${expected}`);
+  }
+  else {
+    console.log(` 👎🏻👎🏻👎🏻 Assertion Failed: , ${actual}, != , ${expected}`);
+  }
+}; */
+
+
+const without = (source, removeSource) => {
+  let withoutArr = source;
+  for(let i = 0; i < source.length; i++) {
+    for(let y = 0; y < removeSource.length; y++) {
+      if(source[i] === removeSource[y]) {
+        withoutArr.splice(i, 1);
+      }
+    }
+  }
+  return withoutArr
+}
+
+module.exports = without;
+
+//console.log(assertArraysEqual(without([1, 2, 3], [1]) => [2, 3]
+//console.log(assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]) => ["1", "2"]
+
+//needs to be refactored will come back to these test cases
