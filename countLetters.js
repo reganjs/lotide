09aa574f45b43e19834d2b2334a81d13cc35e14e
Regan Setter <regansetter@gmail.com>
2022-02-08 15:16:14 -0700

    Created countLetters function for Lotide project

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(` 👍🏻👍🏻👍🏻 Assertion Passed: , ${actual}, = , ${expected}`);
+  } else {
+    console.log(` 👎🏻👎🏻👎🏻 Assertion Failed: , ${actual}, != , ${expected}`);
+  }
+};
+
+const countLetters = (sentence) => {
+  let result = {};
+  for (const allLetters of sentence) {
+    if (allLetters !== " ") {
+      if (result[allLetters]) {
+        result[allLetters] += 1;
+      } else {
+        result[allLetters] = 1;
+      }
+    }
+  }
+  return result;
+}
+
+const testSentence = "This is a test sentence."
+console.log(countLetters(testSentence))
