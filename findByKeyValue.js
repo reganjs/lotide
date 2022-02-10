38ab32b0fbb26fb5b99233996564ed4e146e6bb4
Regan Setter <regansetter@gmail.com>
2022-02-10 10:04:17 -0700

    Created findKeyByValue function

---
+++ findByKeyValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(` 👍🏻👍🏻👍🏻 Assertion Passed: , ${actual}, = , ${expected}`);
+  } else {
+    console.log(` 👎🏻👎🏻👎🏻 Assertion Failed: , ${actual}, != , ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  let objectKey = Object.keys(obj) 
+  for (let i = 0; i < objectKey.length; i++) {
+
+    if (obj[objectKey[i]] === value) {
+      return objectKey[i];
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
