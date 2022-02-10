5ca49849f1cab6d8e04818d1ed258fb7a7883831
Regan Setter <regansetter@gmail.com>
2022-02-10 10:29:13 -0700

    Implemented findKey function for Lotide project

---
+++ eqObjects.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(` 👍🏻👍🏻👍🏻 Assertion Passed: , ${actual}, = , ${expected}`);
+  } else {
+    console.log(` 👎🏻👎🏻👎🏻 Assertion Failed: , ${actual}, != , ${expected}`);
+  }
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+eqObjects(ab, ba); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+eqObjects(ab, abc); // => false
---
+++ findKey.js
@@ -0,0 +1,61 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(` 👍🏻👍🏻👍🏻 Assertion Passed: , ${actual}, = , ${expected}`);
+  } else {
+    console.log(` 👎🏻👎🏻👎🏻 Assertion Failed: , ${actual}, != , ${expected}`);
+  }
+};
+
+const findKey = (object, callback) => {
+  for (let Key in object) {
+    if (callback(object[Key])) {
+      return Key;
+    }
+  }
+  return undefined;
+};
+
+
+assertEqual(findKey({
+  "Blue Hill": {
+    stars: 1
+  },
+  "Akaleri": {
+    stars: 3
+  },
+  "noma": {
+    stars: 2
+  },
+  "elBulli": {
+    stars: 3
+  },
+  "Ora": {
+    stars: 2
+  },
+  "Akelarre": {
+    stars: 3
+  }
+}, x => x.stars === 2), "noma");
+
+console.log(`---`);
+
+assertEqual(findKey({
+  "Blue Hill": {
+    stars: 1
+  },
+  "Akaleri": {
+    stars: 3
+  },
+  "noma": {
+    stars: 2
+  },
+  "elBulli": {
+    stars: 3
+  },
+  "Ora": {
+    stars: 2
+  },
+  "Akelarre": {
+    stars: 3
+  }
+}, x => x.stars === 3), "elBulli");
