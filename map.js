3c5fad45309d17ae9ced54170249b5c36bd06f13
Regan Setter <regansetter@gmail.com>
2022-02-13 21:49:38 -0700

    Implemented map function

---
+++ map.js
@@ -0,0 +1,34 @@
+const eqArrays = (arr1, arr2) => { //
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (actual, expected) => {
+  if (eqArrays(actual, expected)) {
+    console.log(` 👍🏻👍🏻👍🏻 Assertion Passed: , ${actual} === ${expected}`);
+  }
+  else {
+    console.log(` 👎🏻👎🏻👎🏻 Assertion Failed: , ${actual} !== ${expected}`);
+  }
+};
+
+const map = (array, callback) => {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+assertArraysEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't'])
+assertArraysEqual(map(words, word => word[0]), ['g', 'c', 't', 'j', 't'])
+assertArraysEqual(map(words, word => word[1]), ['g', 'c', 'l', 'o', 't'])
+
