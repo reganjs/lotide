a785ea830629ebca8db21269591a341f77c68a29
Regan <52940978+reganjs@users.noreply.github.com>
2022-02-22 11:46:17 -0700

    Deleted previous functions.

--- letterPositions.js
+++ letterPositions.js
@@ -1,20 +1,3 @@
-const eqArrays = (arr1, arr2) => { //
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = (actual, expected) => {
-  if (eqArrays(actual, expected)) {
-    console.log(` 👍🏻👍🏻👍🏻 Assertion Passed: , ${actual} === ${expected}`);
-  } else {
-    console.log(` 👎🏻👎🏻👎🏻 Assertion Failed: , ${actual} !== ${expected}`);
-  }
-};
-
 const letterPositions = function (sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
@@ -32,3 +15,5 @@ const letterPositions = function (sentence) {
 
 const testSentence = "This is a test sentence."
 console.log(letterPositions(testSentence))
+
+module.exports = letterPositions;
