6f1c0e40c93321d8ff11788583b7be531a29a40d
Regan <52940978+reganjs@users.noreply.github.com>
2022-02-22 11:42:14 -0700

    Updated

--- findByKeyValue.js
+++ findByKeyValue.js
@@ -1,11 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(` 👍🏻👍🏻👍🏻 Assertion Passed: , ${actual}, = , ${expected}`);
-  } else {
-    console.log(` 👎🏻👎🏻👎🏻 Assertion Failed: , ${actual}, != , ${expected}`);
-  }
-};
-
 const findKeyByValue = function (obj, value) {
   let objectKey = Object.keys(obj)
   for (let i = 0; i < objectKey.length; i++) {
@@ -14,13 +6,16 @@ const findKeyByValue = function (obj, value) {
       return objectKey[i];
     }
   }
-}
+};
 
-const bestTVShowsByGenre = {
+/* const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama: "The Wire"
 };
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined); 
+*/
+
+module.exports = findKeyByValue;
