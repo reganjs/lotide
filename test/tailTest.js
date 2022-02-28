9503148bfbd7782da36b843089c50f485de06e78
Regan <52940978+reganjs@users.noreply.github.com>
2022-02-28 01:43:10 -0700

    Added tailTest.js

---
+++ test/tailTest.js
@@ -0,0 +1,14 @@
+const assert = require('chai').assert;
+const tail = require('../tail.js');
+
+describe('#tail', () => {
+  it('returns [2, 3] for [1, 2, 3]', () => {
+    assert.deepEqual(tail([1, 2, 3]),[2, 3]);
+  });
+  it('returns [2, 3, 4] for [1, 2, 3, 4]', () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+  });
+  it('returns [] for []', () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
