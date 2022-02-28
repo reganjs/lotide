7aacf1453f565a37c5ce6eeb439c235dca7a28c7
Regan <52940978+reganjs@users.noreply.github.com>
2022-02-28 01:28:29 -0700

    eqArraysTest.js added

---
+++ test/eqArraysTest.js
@@ -0,0 +1,23 @@
+const assert = require('chai').assert;
+const eqArrays = require('../eqArrays')
+
+describe("#eqArrays", () => {
+
+  it("returns true for [1, 2, 3], [1, 2, 3]", () => {
+  assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3]));
+  });
+
+  it("returns false for [1, 2, 3], [3, 2, 1]", () => {
+  assert.isFalse((eqArrays([1, 2, 3], [3, 2, 1])));
+  });
+
+  it("returns true for [\"1\", \"2\", \"3\"], [\"1\", \"2\", \"3\"]", () => {
+  assert.isTrue(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+  });
+
+  it("returns false for [\"1\", \"2\", \"3\"], [\"1\", \"2\", 3]", () => {
+  assert.isFalse(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+  });
+  
+//console.log(eqArrays([1, 2, 3], [1, 2, 3]))
+//console.log(eqArrays([1, 2, 3], [3, 2, 1]))
